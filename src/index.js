a159b81c1594773c4bf3430a88dbff5cfd44d282
Roberta Barberena <barberenar@gmail.com>
2022-03-15 19:25:53 -0400

    Change temperature unit function and styles added

--- src/index.js
+++ src/index.js
@@ -14,23 +14,6 @@ let minutes = date.getMinutes();
 let dateTime = document.querySelector("#time");
 dateTime.innerHTML = `${day}, ${hour}:${minutes}`;
 
-function searchCity(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city");
-  let cityInput = city.value;
-  let cityTitle = document.querySelector("#city-title");
-  cityTitle.innerHTML = cityInput.charAt(0).toUpperCase() + cityInput.slice(1);
-  let apiKey = "21475e7ebad3c82a3f3c6e1dfcd1aad7";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${apiKey}`;
-  axios.get(apiUrl).then(showCityTemp);
-}
-
-function showCityTemp(response) {
-  let temp = Math.round(response.data.main.temp);
-  let temperature = document.querySelector("#temperature");
-  temperature.innerHTML = temp;
-}
-
 function getCurrentPosition(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchCurrentCity);
@@ -41,19 +24,52 @@ function searchCurrentCity(position) {
   let longitude = position.coords.longitude;
   let apiKey = "21475e7ebad3c82a3f3c6e1dfcd1aad7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showCurrentCityTemp);
+  axios.get(apiUrl).then(showCityTemp);
 }
 
-function showCurrentCityTemp(response) {
-  let temp = Math.round(response.data.main.temp);
+function searchCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city");
+  let cityInput = city.value;
+  //let cityTitle = document.querySelector("#city-title");
+  //cityTitle.innerHTML = cityInput.charAt(0).toUpperCase() + cityInput.slice(1);
+  let apiKey = "21475e7ebad3c82a3f3c6e1dfcd1aad7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showCityTemp);
+}
+
+function showCityTemp(response) {
+  celsiusTemp = response.data.main.temp;
+  let temp = Math.round(celsiusTemp);
   let temperature = document.querySelector("#temperature");
   temperature.innerHTML = temp;
   let city = response.data.name;
   let cityTitle = document.querySelector("#city-title");
   cityTitle.innerHTML = city;
 }
+function tempToFarenh(event) {
+  let farenhTemp = (celsiusTemp * 9 / 5) + 32;
+  let temp = Math.round(farenhTemp);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = temp;
+  farenhLink.classList.remove("active");
+  celsiusLink.classList.add("active");
+}
 
+function tempToCelsius(event) {
+  let temp = Math.round(celsiusTemp);
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = temp;
+  celsiusLink.classList.remove("active");
+  farenhLink.classList.add("active");
+}
+
+let celsiusTemp = null;
 let searchButton = document.querySelector("#search");
 searchButton.addEventListener("click", searchCity);
 let geoButton = document.querySelector("#geoTarget");
 geoButton.addEventListener("click", getCurrentPosition);
+let farenhLink = document.querySelector("#farenh-link");
+farenhLink.addEventListener("click", tempToFarenh);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", tempToCelsius);
